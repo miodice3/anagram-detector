5a8c3320d1c14642612906d5d79375d3ca84163e
Michael Iodice <miodice3@gmail.com>
2020-10-08 11:44:59 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+def match(array_of_possible_anagrams)
+    return_array = []
+    array_of_possible_anagrams.each do |each_word|
+#        binding.pry
+        if each_word.chars.sort.join == @word.chars.sort.join
+            return_array << each_word
+        end
+    end
+    return_array
+end
+
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
